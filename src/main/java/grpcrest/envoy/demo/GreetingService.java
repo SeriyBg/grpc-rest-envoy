043308379aec0aad701a10bbfa8d3565777cf6ed
Serge Bishyr <seriybg@gmail.com>
2020-03-09 15:55:28 +0200

    add response method

--- src/main/java/grpcrest/envoy/demo/GreetingService.java
+++ src/main/java/grpcrest/envoy/demo/GreetingService.java
@@ -2,6 +2,7 @@ package grpcrest.envoy.demo;
 
 import com.google.protobuf.Empty;
 import grpcrest.envoy.demo.proto.Greeting;
+import grpcrest.envoy.demo.proto.GreetingResponse;
 import grpcrest.envoy.demo.proto.GreetingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
@@ -13,6 +14,15 @@ public class GreetingService extends GreetingServiceGrpc.GreetingServiceImplBase
     public void greet(Greeting request, StreamObserver<Empty> responseObserver) {
         System.out.println("Hello " + request.getHello());
         responseObserver.onNext(Empty.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void greetResponse(Greeting request, StreamObserver<GreetingResponse> responseObserver) {
+        responseObserver.onNext(GreetingResponse.newBuilder()
+                .setHello("Hello")
+                .setName(request.getHello())
+                .build());
         responseObserver.onCompleted();
     }
 }
